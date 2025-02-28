d807c2fd1f1a2e5bcee9ad0961d8d43be48b0891
Lunnaris01 <lunnaris01@googlemail.com>
2025-02-28 20:44:18 +0100

    attatching simple surfaces

--- space shooter/main.py
+++ space shooter/main.py
@@ -10,6 +10,15 @@ pygame.init()
 display_surface = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption("Space Shooter")
 
+# plain surface
+surf = pygame.Surface((100,200))
+surf.fill('orange')
+x = 100
+
+# surface from image
+player_surf = pygame.image.load('images/player.png')
+
+
 running = True
 while running:
 
@@ -20,7 +29,13 @@ while running:
 
     #draw the game
 
-    display_surface.fill("green")
+    display_surface.fill("gray")
+
+    display_surface.blit(surf,(x,150)) #block image transfer
+    if x<500:
+        x += 0.05
+
+    display_surface.blit(player_surf,(600,300))
     pygame.display.update()
 
 

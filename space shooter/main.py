88d35743b32e3c495c3759b26e9744f3d5386ef1
Lunnaris01 <lunnaris01@googlemail.com>
2025-03-01 00:34:56 +0100

    delta time and framerate fix

--- space shooter/main.py
+++ space shooter/main.py
@@ -12,6 +12,12 @@ pygame.init()
 display_surface = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption("Space Shooter")
 
+clock = pygame.time.Clock()
+dt = 16
+
+running = True
+
+
 # plain surface
 surf = pygame.Surface((100,200))
 surf.fill('orange')
@@ -21,6 +27,7 @@ display_surface.blit(surf,(x,150)) # moved outside the loop thus will be overwri
 # surfaces and rectangles from imagefiles
 player_surf = pygame.image.load(path.join('images','player.png')).convert_alpha()
 player_rect = player_surf.get_frect(midbottom = (WIN_WIDTH //2 , WIN_HEIGHT-10))
+player_direction = pygame.math.Vector2((1,1))
 
 star_surf = pygame.image.load(path.join('images','star.png')).convert_alpha()
 star_positions = [(random.randint(0,WIN_WIDTH-1),random.randint(0,WIN_HEIGHT-1)) for _ in range(20)]
@@ -34,8 +41,6 @@ laser_rect = laser_surf.get_frect(bottomleft = (20,WIN_HEIGHT-20))
 # We can create plain (F)rects as well.
 
 
-running = True
-player_movement = 0.25
 while running:
 
     # Event loop
@@ -52,13 +57,18 @@ while running:
 
     # Player Movement
     if (player_rect.right > WIN_WIDTH or player_rect.left<0):
-        player_movement *= -1
-    player_rect.right += player_movement
+        player_direction.x *= -1
+    if (player_rect.bottom > WIN_HEIGHT or player_rect.top<0):
+        player_direction.y *= -1
+    player_rect.center += player_direction*dt/16
 
     display_surface.blit(meteor_surf,meteor_rect)
     display_surface.blit(laser_surf,laser_rect)
     display_surface.blit(player_surf,player_rect)
     pygame.display.update()
+
+    dt = clock.tick()
+    print(dt,clock.get_fps())
 
 
 pygame.quit()

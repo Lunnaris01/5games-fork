94b106efbd34f2dc52b9e61d75b15057adaff6ab
Lunnaris01 <lunnaris01@googlemail.com>
2025-03-01 17:36:54 +0100

    add Dockerfile and requirements.txt

--- space shooter/main.py
+++ space shooter/main.py
@@ -1,6 +1,7 @@
 import pygame
 from os import path
 import random
+import numpy as np
 
 # CONSTS
 
@@ -13,7 +14,7 @@ class Player(pygame.sprite.Sprite):
         super().__init__(*groups)
         self.image = pygame.image.load(path.join('images','player.png')).convert_alpha()
         self.rect = self.image.get_frect(midbottom = (WIN_WIDTH //2-50 , WIN_HEIGHT-5))
-        self.player_speed = 100
+        self.player_speed = 250
         self.direction = pygame.Vector2()
 
         # gun cooldown
@@ -32,8 +33,13 @@ class Player(pygame.sprite.Sprite):
         self.direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
         self.direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
         self.rect.center += self.direction *dt*self.player_speed
-        if keys_justpressed[pygame.K_SPACE] and self.can_shoot:
-            Laser(laser_surf,self.rect.midtop,all_sprites)
+        # clip position to remain in game window
+        self.rect.left = np.clip(self.rect.left,0,WIN_WIDTH-self.rect.width)
+        self.rect.top = np.clip(self.rect.top,0,WIN_HEIGHT-self.rect.height)
+
+
+        if keys[pygame.K_SPACE] and self.can_shoot:
+            Laser(laser_surf,self.rect.midtop,all_sprites,laser_sprites)
             self.can_shoot = False
             self.laser_shoot_time = pygame.time.get_ticks()
         self.laser_timer()
@@ -49,12 +55,16 @@ class Meteor(pygame.sprite.Sprite):
         super().__init__(*groups)
         self.image = surf
         if pos == None:
-            pos = (random.randint(200,WIN_WIDTH-200),random.randint(0,WIN_HEIGHT-200))
+            pos = (random.randint(100,WIN_WIDTH-100),random.randint(-200,0))
         self.rect = self.image.get_frect(center = pos)
         self.creationtime = pygame.time.get_ticks()
+        self.direction = pygame.Vector2(random.randint(-500,500)/1000,1)
+        self.speed = random.randint(250,550)
+        
     
     def update(self, key, keys_justpressed,dt):
-        if pygame.time.get_ticks()-self.creationtime>2000:
+        self.rect.center += self.direction * self.speed * dt
+        if pygame.time.get_ticks()-self.creationtime>2500:
             self.kill()
 
 
@@ -71,6 +81,11 @@ class Laser(pygame.sprite.Sprite):
         if self.rect.bottom < 0:
             self.kill()
 
+def collisions():
+    if (pygame.sprite.spritecollide(player,meteor_sprites,False)):
+        return False
+    pygame.sprite.groupcollide(laser_sprites,meteor_sprites,True,True)
+    return True
 
 # general setup
 pygame.init()
@@ -91,14 +106,17 @@ laser_surf = pygame.image.load(path.join('images','laser.png')).convert_alpha()
 meteor_surf = pygame.image.load(path.join('images','meteor.png')).convert_alpha()
 
 all_sprites = pygame.sprite.Group()
+meteor_sprites = pygame.sprite.Group()
+laser_sprites = pygame.sprite.Group()
+
 stars = [Star(star_surf,all_sprites) for _ in range(20)]
-meteor = Meteor(meteor_surf,(WIN_WIDTH //2 , WIN_HEIGHT //2),all_sprites)
 player = Player(all_sprites)
 
 
 # custom events -> meteor spawning
 meteor_event = pygame.event.custom_type()
-pygame.time.set_timer(meteor_event,500)
+meteor_spawn_cooldown = 500
+pygame.time.set_timer(meteor_event,meteor_spawn_cooldown)
 
 while running:
 
@@ -110,7 +128,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == meteor_event:
-            Meteor(meteor_surf, None ,all_sprites)
+            Meteor(meteor_surf, None ,all_sprites, meteor_sprites)
 
 
 
@@ -124,9 +142,23 @@ while running:
     # Sprites will be drawn in order they were inserted!
     all_sprites.draw(display_surface)
 
+
+    # Check Collisions
+    running = collisions()
+    #for l_sprite in laser_sprites:
+    #    collided_sprites = pygame.sprite.spritecollide(l_sprite,meteor_spritesd,True)
+    #    if collided_sprites:
+    #        l_sprite.kill()
+
     pygame.display.update()
     dt = clock.tick() / 1000
 
+    # Preparations for next run
 
+    if pygame.time.get_ticks()%1000 == 0:
+        meteor_spawn_cooldown = meteor_spawn_cooldown*0.99
+        pygame.time.set_timer(meteor_event,int(meteor_spawn_cooldown))
+
+print(f"Score: {pygame.time.get_ticks()//1000}")
 
 pygame.quit()
